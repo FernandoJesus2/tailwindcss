896bd3d03aa4e7c557200829caa889a65bbddfc8
FernandoJesus2 <fernando.fj02@hotmail.com>
2022-03-19 21:32:42 -0300

    Primeira tela usando o tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+const purgecs = required('@fullhuman/postcss-purgecss')({
+content:[
+    './src/**/*.html',
+],
+
+defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+
+module.exports = {
+    plugins: [
+        require ('tailwindcss'),
+        purgecss,
+        require ('cssnano'),
+        require ('autoprefixer'),
+    ]
+  }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+
+  safelist: [{pattern: /^.*$/}],
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
